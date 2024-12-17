46740f516da77ed0ac161e2dfc8b42fb9057d0d1
nyaru177 <1770221228@qq.com>
2024-12-17 22:04:55 +0800

    3020

--- init-db.sh
+++ init-db.sh
@@ -9,3 +9,7 @@ done
 # 执行数据库初始化
 echo "Initializing database..."
 mysql -h mysql-container -u root -prootpassword auth_system_db < /app/db_init/init.sql
+
+# 启动 Express 应用
+echo "Starting Express application..."
+yarn start
